f03795b6e16844279417f9fffb49761ac34eea55
MohamedAitHamoucha <aithamoucha.mohamed@gmail.com>
2023-08-16 18:06:59 +0100

    TrustTech

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,11 @@ use App\Http\Controllers\UserController;
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
+
+Route::prefix('api')->group(function () {
+    // Add a Fournisseur
+    Route::post('addFournisseur', [FournisseursController::class, 'addFournisseur']);
+    Route::put('updateFournisseur/{nom}', [FournisseursController::class, 'updateFournisseur']);
+    Route::delete('deleteFournisseur', [FournisseursController::class, 'deleteFournisseur']);
+    Route::get('getFournisseurDetails', [FournisseursController::class, 'getFournisseurDetails']);
+});
